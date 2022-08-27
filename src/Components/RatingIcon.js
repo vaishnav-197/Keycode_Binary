5a4de8fe9eab7170f6d51a633d716fc3be0bab04
Gokul K <gokul.k@keyvalue.systems>
2022-08-28 02:44:26 +0530

    Remove icons from rating

--- src/Components/RatingIcon.js
+++ src/Components/RatingIcon.js
@@ -1,22 +1,25 @@
 import React from 'react'
-import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome5'
-import Ionicon from 'react-native-vector-icons/Ionicons'
-import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
-import EntrypoIcon from 'react-native-vector-icons/Entypo'
+import { Text } from 'react-native'
+
+const getColor = (rating) => {
+  switch(rating) {
+    case 1:
+      return 'red'
+    case 2:
+      return '#dc143c'
+    case 3:
+      return 'orange'
+    case 4:
+      return 'lightgreen'
+    case 5:
+      return 'green'
+  }
+}
 
 const RatingIcon = ({ rating }) => {
-  switch (rating) {
-    case 1:
-      return <FontAwesomeIcon name="sad-cry" color={'red'} size={24} />
-    case 2:
-      return <Ionicon name="sad" color={'#dc143c'} size={24} />
-    case 3:
-      return <MaterialIcon name="face" color={'orange'} size={24} />
-    case 4:
-      return <EntrypoIcon name="emoji-happy" color={'lightgreen'} size={24} />
-    case 5:
-      return <Ionicon name="happy-outline" color={'green'} size={24} />
-  }
+  return (
+    <Text>{ rating }</Text>
+  )
 }
 
 export default RatingIcon
