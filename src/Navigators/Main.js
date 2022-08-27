5e100f2b8a642dab27a6192fb43dfb58e5c54806
nithya <nithya@keyvalue.systems>
2022-08-27 09:30:57 +0530

    first commit

---
+++ src/Navigators/Main.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { ExampleContainer } from '@/Containers'
+
+const Tab = createBottomTabNavigator()
+
+// @refresh reset
+const MainNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={ExampleContainer}
+        options={{
+          tabBarIconStyle: { display: 'none' },
+          tabBarLabelPosition: 'beside-icon',
+        }}
+      />
+      <Tab.Screen
+        name="house"
+        component={ExampleContainer}
+        options={{
+          tabBarIconStyle: { display: 'none' },
+          tabBarLabelPosition: 'beside-icon',
+        }}
+      />
+            <Tab.Screen
+        name="asasd"
+        component={ExampleContainer}
+        options={{
+          tabBarIconStyle: { display: 'none' },
+          tabBarLabelPosition: 'beside-icon',
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
+
+export default MainNavigator
