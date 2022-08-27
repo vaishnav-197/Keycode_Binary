5ec6c2686cebf2e2f3cb423dcbe4e7871f86b605
rohithnair1996 <rohithmaccs@gmail.com>
2022-08-27 15:56:41 +0530

    Added venue template

--- src/Containers/VenueScreen.js
+++ src/Containers/VenueScreen.js
@@ -9,6 +9,8 @@ import {
 } from 'react-native'
 import { useDispatch } from 'react-redux'
 import { useTranslation } from 'react-i18next'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
 import { Brand } from '@/Components'
 import { useTheme } from '@/Hooks'
 import { useLazyFetchOneQuery } from '@/Services/modules/users'
@@ -17,19 +19,43 @@ import ListCard from '@/Components/ListCard'
 import SuggestionCard from '@/Components/SuggestionCard'
 import BottomNavigationBar from '../Components/BottomNavigationBar'
 import AppBar from '../Components/AppBar'
-import SearchBar from '../Components/SearchBar'
+import SearchBar from '../Components/searchBar'
 
 const VenueScreen = () => {
   return (
     <View>
       <AppBar title={'Venue'}/>
-      <SearchBar />
+      <View style={styles.header}>
+        <View style={styles.searchWrapper}>
+        <SearchBar />
+        </View>
+        <View style={styles.iconWrapepr}>
+        <Icon 
+          name="filter-list" 
+          color={'black'} 
+          size={20} 
+        />
+        </View>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   topBanner:{},
+  header:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  iconWrapepr: {
+    width: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  searchWrapper: {
+    flex: 1
+  }
 })
 
 export default VenueScreen;
